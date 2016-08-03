d2c0509f9115269a3a5dcd7167fbcb6efd634e3f
VijayVyas <vijay.bhilwara@gmail.com>
2016-08-04 03:46:21 +0530

    heroku database reupdate

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'This is some sample text fro our blog from controller';
+		@content_second = 'This is more sample text fro our blog from controller';
+	end
+	
+	def new
+
+	end
+	
+	def create
+
+	end
+	
+	def edit
+
+	end
+	
+	def update
+
+	end
+	
+	def show
+
+	end
+	
+	def destroy
+
+	end
+
+end
