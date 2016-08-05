7fb4471116f1ae3b209c8f50907034627bee68dd
VijayVyas <vijay.bhilwara@gmail.com>
2016-08-05 18:58:40 +0530

    add category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def index
+	@categories = Category.all
   end
 
   def edit
@@ -9,5 +10,21 @@ class CategoriesController < ApplicationController
   end
 
   def new
+	@category = Category.new
   end
+  def create
+	@category = Category.new(category_params)
+	   puts "category id is "+ @category_id.to_s
+		if(@category.save)
+			redirect_to categories_path, :notice => "Your Category has been saved"
+		else
+			render "new"
+		end
+  end
+  
+  private
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def category_params
+            params.require(:category).permit(:name)
+        end
 end
