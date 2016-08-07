c4b168f1b99b49c7a86fb767cdeeaed94d0629cc
VijayVyas <vijay.bhilwara@gmail.com>
2016-08-08 00:29:35 +0530

    change permit_params for admin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Post do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :body, :category_id, :author_id
 #
 # or
 #
@@ -12,6 +12,5 @@ ActiveAdmin.register Post do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
 
 end
